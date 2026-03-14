cc899bed9fa0f47beef31ba4cc656e3485cf5d05
Dakto <dakto@daktoinc.co.uk>
2026-03-14 19:22:49 +0100

    Update home/index.php

--- home/index.php
+++ home/index.php
@@ -10,21 +10,21 @@ echo(navbar());
         <div class="settings-container-top">
                 <h1 class="qvhtile">Novymap project</h1><h1 class="qvhtilesmall">  Forked from QVH by Dakto.</h1>
                 <div class="feature-grid">
-                    <div onclick="window.location.assign(&quot;https://map.novymap-qvh.top/&quot;)" href="https://novymap.daktoinc.co.uk/" class="feature-card">
+                    <div onclick="window.location.assign(&quot;https://novymap.daktoinc.co.uk/&quot;)" href="https://novymap.daktoinc.co.uk/" class="feature-card">
                         <h3>Interactive Map</h3>
                         <p>Discover and explore points of interest on our interactive map.</p>
                     </div>
                     <div onclick="window.location.assign(&quot;/credits&quot;)"  class="feature-card">
                         <h3>Credits</h3>
-                        <p>Learn about the mediocre people behind Novymap-qvh.</p>
+                        <p>Learn about the mediocre people behind Novymap.</p>
                     </div>
  <div onclick="window.location.assign(&quot;https://novyadmin.daktoinc.co.uk/&quot;)" class="feature-card">
                         <h3><del>Admin Panel</del></h3>
-                        <p>Admin stuffs</p>
+                        <p>There is no Admin Panel for now.</p>
                     </div>
 <div onclick="window.location.assign(&quot;https://discord.gg/NSdnxsjA8y&quot;)"  class="feature-card">
                         <h3>Discord</h3>
-                        <p>Do we miss any dail?Let us know in discord.</p>
+                        <p>Did we miss any dials? Contact us on the Discord server!</p>
                     </div>
 
 <div onclick="window.location.assign(&quot;https://discord.com/oauth2/authorize?client_id=1413462775581114468&quot;)"  class="feature-card">
@@ -48,11 +48,7 @@ echo(navbar());
                 </div>
 
 
-  <div class="settings-container-top">
-                <h1 class="qvhtile">BTW I use Arch :3</h1>
-<h1 class="qvhtilesmall">Also PHP and Nginx :3</h1>
 
-<?php echo(neofetch(true,$conn));?>
               </div>
               </div>
 
