c7b79020aa35b20b4a961adf0932fd01a330b5d6
Dakto <dakto@daktoinc.co.uk>
2026-03-15 01:35:25 +0100

    Update shared/navbar.php

--- shared/navbar.php
+++ shared/navbar.php
@@ -9,6 +9,7 @@ $isLoggedIn = isset($_SESSION['discord_access_token']);
     <input type="checkbox" id="nav-check">
     <div class="nav-header">
         <div class="nav-title">
+            <link rel="icon" type="image/png" href="/img/novymap-qvh.png">
             novymap
         </div>
     </div>
